ce689d0d25ec241b74300e5133431bb083e1f1b7
eduyl <proman312008@gmail.com>
2024-06-28 15:35:56 +0530

    edit fair.c

--- KernelCompile.sh
+++ KernelCompile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /media/ian/SeagateHDD/android/google/common
-ARCH=arm CROSS_COMPILE=/media/ian/CrucialP5_SSD/android/lineage-18-gts210ltexx/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel- make mrproper O="/media/ian/SeagateHDD/KernelOut"
-ARCH=arm CROSS_COMPILE=/media/ian/CrucialP5_SSD/android/lineage-18-gts210ltexx/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel- make lineage_gts210ltexx_defconfig zImage dtbs modules -j4 O="/media/ian/SeagateHDD/KernelOut"
+cd /media/jan/android/kernel-4.19
+ARCH=arm CROSS_COMPILE=/media/jan/android/LOS-UL/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel- make mrproper O="/media/jan/android/KernelOut"
+ARCH=arm CROSS_COMPILE=/media/jan/android/LOS-UL/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel- make lineage_gts210ltexx_defconfig zImage dtbs modules -j4 O="/media/jan/android/KernelOut"
